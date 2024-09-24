2e3bb801bfcd038e634547048e25503f215a3d4a
Srinivasansoundar <srinisoundar66@gmail.com>
2024-09-24 23:00:02 +0530

    created sign-in api route

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,5 +1,6 @@
 const User = require("../modal/user.modal.js")
 const bcryptjs=require("bcryptjs")
+const jwt=require("jsonwebtoken")
 const {errorHandler}=require("../utils/error.js")
 module.exports.signup = async (req, res,next) => {
    const { username, email, password } = req.body;
@@ -20,5 +21,27 @@ module.exports.signup = async (req, res,next) => {
    }
    catch(err){
       next(err)
+   }
+}
+module.exports.signin=async(req,res,next)=>{
+   const {email,password}=req.body;
+   if(!email || !password || email==='' ||password===''){
+      next(errorHandler(400,"All fields are required"));
+   }
+   try{
+      const validUser=await User.findOne({email})
+      if(!validUser){
+        return  next(errorHandler(404,"User not found"));
+      }
+      const validPassword=bcryptjs.compareSync(password,validUser.password);
+      if(!validPassword){
+        return  next(errorHandler(400,"Invalid password"))
+      }
+      const token=jwt.sign({id:validUser._id},process.env.JWT_SECRET)
+      const {password:pass,...rest}=validUser._doc;
+      res.status(200).cookie('access_token',token,{httpOnly:true}).json(rest)
+   }
+   catch(err){
+         next(err)
    }
 }
--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const express=require("express")
-const {signup}=require("../controllers/auth.controllers.js")
+const {signup,signin}=require("../controllers/auth.controllers.js")
 const router=express.Router();
 router.post("/sign-up",signup);
+router.post("/sign-in",signin)
 module.exports=router;
