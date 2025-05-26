60ed2b5c63f0b15a5c8dccd6de9370a0e4293530
Srinivasansoundar <srinisoundar66@gmail.com>
2025-05-26 13:12:40 +0530

    create get user api route

--- api/controllers/user.controllers.js
+++ api/controllers/user.controllers.js
@@ -68,3 +68,39 @@ module.exports.signout=(req,res,next)=>{
     next(err);
   }
 }
+module.exports.getUser=async(req,res,next)=>{
+  if(!req.user.isAdmin){
+      return next(errorHandler(403,'You are not allowed to see all the users'))
+    }
+  try{
+    const startIndex=parseInt(req.query.startIndex) || 0
+    const limit=parseInt(req.query.limit) || 9
+    const sortDirection=req.query.sort==='asc'?1:-1
+    const users=await User.find()
+    .sort({createdAT:sortDirection})
+    .skip(startIndex)
+    .limit(limit)
+    const userWithoutPassword=users.map((user)=>{
+      const {password:pass,...rest}=user._doc
+      return rest;
+    })
+    const totalUsers=await User.countDocuments();
+    const now=new Date();
+    const oneMonthAgo=new Date(
+      now.getFullYear(),
+      now.getMonth()-1,
+      now.getDate()
+    )
+    const lastMonthUsers=await User.countDocuments({
+      createAt:{$gte:oneMonthAgo}
+    });
+    res.status(200).json({
+      users:userWithoutPassword,
+      totalUsers,
+      lastMonthUsers,
+    })
+  }
+  catch(err){
+    next(err)
+  }
+}
--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -2,9 +2,10 @@ const express=require("express");
 const {test}=require("../controllers/user.controllers")
 const router=express.Router();
 const {verifyToken}=require("../utils/verifyUser")
-const {updateUser,deleteUser,signout}=require("../controllers/user.controllers")
+const {updateUser,deleteUser,signout,getUser}=require("../controllers/user.controllers")
 router.get("/test",test)
 router.put('/update/:userId',verifyToken,updateUser)
 router.delete('/delete/:userId',verifyToken,deleteUser)
 router.post('/signout',signout)
+router.get('/getusers',verifyToken,getUser)
 module.exports=router;
